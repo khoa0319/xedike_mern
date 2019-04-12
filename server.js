6fa88305be9ef8144596321cf1e0def0bd90ead3
khoa0319 <khoa0319@gmail.com>
2019-04-12 19:48:50 +0700

    initialize server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+/* 3rd packages */
+const express = require('express');
+
+/* project packages */
+
+/* Initialize server */
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server's listening on PORT: " + port);
+  console.log('Connected to DB');
+})
