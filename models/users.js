53064d65e50d8ede62a42cd21587134db82e4b94
khoa0319 <khoa0319@gmail.com>
2019-04-24 20:10:59 +0700

    fix users.js

--- models/users.js
+++ models/users.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
   // cannot modified: email, id, phone, usertype
   email: { type: String, required: true },
-  password: String, 
-  fullName: String,
+  password: {type: String, required: true }, 
+  fullName: {type: String, required: true },
   phone: String,
   userType: { type: String, required: true },
-  dateOfBirth: {type: Date},
-  registerDate: {type: Date, default: new Date()},
+  dateOfBirth: { type: Date },
+  registerDate: { type: Date, default: new Date() },
   numberOfTrips: { type: Number, default: 0 },
   numberOfKms: Number,
   avatar: String,
